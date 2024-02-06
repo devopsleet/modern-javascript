1c8d6bbab80ac311f91bc3661b78d043a09a4655
gagan <gasingla07@gmail.com>
2024-02-05 20:26:28 -0600

    link a JS file part 3

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -3,3 +3,12 @@ if (js === "amazing") alert("Javascript is Fun");
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+
+
+// declaring a variable
+let firstName = "Jonas"
+console.log(firstName);
+
+// let 5years = 5;
