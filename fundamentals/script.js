82189240d6aeb0a34b2673842f2a32ded2e49cfa
gagan <gasingla07@gmail.com>
2024-02-07 21:45:27 -0600

    data types

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,14 +1,32 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is Fun");
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is Fun");
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
-
+console.log("Jonas");
 
 // declaring a variable
-let firstName = "Jonas"
+let firstName = "Jonas";
 console.log(firstName);
 
 // let 5years = 5;
+
+true;
+console.log(true);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Gagan');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
