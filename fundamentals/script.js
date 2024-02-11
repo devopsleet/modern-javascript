b1062ce6ae70571c500d4d1fcba8066eb5635915
gagan <gasingla07@gmail.com>
2024-02-11 15:25:41 -0600

    strings and template literals

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,32 +1,57 @@
 // let js = "amazing";
 // if (js === "amazing") alert("Javascript is Fun");
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
+// console.log("Jonas");
 
-// declaring a variable
-let firstName = "Jonas";
-console.log(firstName);
+// // declaring a variable
+// let firstName = "Jonas";
+// console.log(firstName);
 
-// let 5years = 5;
+// // let 5years = 5;
 
-true;
-console.log(true);
+// true;
+// console.log(true);
 
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof 'Gagan');
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Gagan');
 
-javascriptIsFun = 'YES';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES';
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(typeof year);
+// let year;
+// console.log(typeof year);
+
+// const ageJonas = 2037 - 1991;
+// console.log(ageJonas);
+
+// const ageSarah = 2037 - 2018;
+// console.log(ageSarah);
+
+// console.log(ageJonas, ageSarah);
+
+const firstName = 'Jonas';
+const job = 'techer';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ' ,a ' +  (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old teacher `;
+console.log(jonasNew);
+
+console.log('String with \n\n');
+console.log(`String with
+multiple lines`);
+
 
