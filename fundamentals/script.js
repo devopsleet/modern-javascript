25270473947638c2e026a812ef910b47aadf7875
gagan <gasingla07@gmail.com>
2024-11-19 15:41:46 -0600

    Array concepts

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -34,3 +34,66 @@ function fruitProcessor(apples, oranges) {
 const ans = fruitProcessor(2,3);
 console.log(ans);
 console.log();
+
+console.log(Number('23'));
+console.log('23');
+
+
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear;
+
+  return age;
+
+
+}
+
+const age1 = calcAge1(1991);
+
+// anonymous function
+// function expression
+const calcAge2 = function(birthYear) {
+  return 2037 - birthYear
+}
+
+const age2 = calcAge2(2037);
+
+console.log(age2);
+
+// Arrow functions
+// Function expression
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const yearsUntilretiremenet = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retirement`;
+}
+
+console.log( yearsUntilretiremenet(1991, 'Gagan'));
+console.log(yearsUntilretiremenet(2012, "abc"));
+
+
+// Arrays
+const friend1 = 'Michael1';
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2020);
+
+console.log(friends[1]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Gagan';
+
+const jonas = [firstName, 'Singla', 2037 - 1991, friends]
+console.log(jonas);
+
