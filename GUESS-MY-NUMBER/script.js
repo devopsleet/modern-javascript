f38decab125b8d4067dfccb3cb4c739850db0697
gagan <gasingla07@gmail.com>
2024-11-21 16:42:42 -0600

    handling click events

--- GUESS-MY-NUMBER/script.js
+++ GUESS-MY-NUMBER/script.js
@@ -8,3 +8,14 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 10
 
 */
+
+const number = Math.random();
+
+
+
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  !guess ? console.log('No number') : console.log(guess);
+});
