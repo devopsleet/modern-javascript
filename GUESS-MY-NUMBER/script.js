6e003fd45502ba538189d390e2c6d5d435388b6c
gagan <gasingla07@gmail.com>
2024-11-25 16:28:52 -0600

    scoping in practice

--- GUESS-MY-NUMBER/script.js
+++ GUESS-MY-NUMBER/script.js
@@ -1,21 +1,58 @@
 'use strict';
-/*
-
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number'
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 10
-
-*/
-
-const number = Math.random();
 
 
+function calcAge(birthyear) {
+  const age = 2037 - birthyear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthyear} `;
+    console.log(output);
+
+    if(birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true;
+      const str = `Oh, you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a,b) {
+        return a + b;
+      }
+
+    }
+    console.log(millenial);
+    add(2,3);
+  }
+  //console.log(str);
+  
+  printAge()
+  return age;
+
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+//console.log(age);
+
+// /*
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number'
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 10
+
+// */
+
+// const number = Math.random();
 
 
-document.querySelector('.check').addEventListener('click', () => {
-  const guess = Number(document.querySelector('.guess').value);
 
-  !guess ? console.log('No number') : console.log(guess);
-});
+
+// document.querySelector('.check').addEventListener('click', () => {
+//   const guess = Number(document.querySelector('.guess').value);
+
+//   !guess ? console.log('No number') : console.log(guess);
+// });
+
+
+
