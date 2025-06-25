5d7e8c2802e7c5fcc08d1fb9dc17bd7ea2a835b0
gagan <gasingla07@gmail.com>
2025-06-25 10:17:33 -0500

    variables

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,44 +1,15 @@
-const jonas = {
-  firstName : "Jonas",
-  lastName : "Singla",
-  age : 2037 - 1991,
-  job : 'teacher',
-  friends : ['Michael', 'Peter', 'Steven'],
-  hasDriversLicense : false,
-  birthYear: 1998,
-  calcAge: function() {
-    console.log(this);
-    return 2037 - this.birthYear;
-  }
+const feelInMile = 5280;
 
-};
-
-console.log(`${jonas.firstName} is ${jonas.age} years old and he ${jonas.hasDriversLicense ? 'has a' : 'no'} driver's license`);
-
-// for loop keeps runnning while condition is TRUE
-
-for(let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights repetition`);
+function example() {
+  console.log(a);
+  var a = 10;
 }
 
-for(let i = 0; i < 10; i++) {
-  console.log('i');
-}
-// console.log(jonas.calcAge(1991));
+example();
 
-
-// console.log(jonas.lastName);
-// console.log(jonas['lastName']);
-
-// const nameKey = 'Name';
-
-// console.log(jonas['first' + nameKey]);
-
-// // const interestedIn = prompt('What do you wnt to know about Jonas?')
-// // console.log(jonas[interestedIn]);
-
-// // if(jonas[interestedIn]) {
-// //   console.log(jonas[interestedIn]);
-// // }
-
-// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`);
+// use of const with arrays
+// const allows to mutate the arrays or objects
+const arr = [1, 2, 3];
+console.log(`Array before push ${arr}`);
+arr.push(5);
+console.log(`Array after push ${arr}`);
