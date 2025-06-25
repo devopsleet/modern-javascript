9b1945b5e9864337dde35e2038a8c1c08682112f
gagan <gasingla07@gmail.com>
2025-06-25 14:21:28 -0500

    string methods

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -9,7 +9,48 @@ example();
 
 // use of const with arrays
 // const allows to mutate the arrays or objects
-const arr = [1, 2, 3];
+let arr = [1, 2, 3];
 console.log(`Array before push ${arr}`);
 arr.push(5);
 console.log(`Array after push ${arr}`);
+
+let age = 64;
+console.log(age++);
+console.log(++age);
+let isActivegame = true;
+let isLoggedIn = false;
+let isGameOver = 1;
+
+let $age = 9;
+
+// Strings
+
+let username = "Gagan";
+let password = "12123233";
+
+console.log(' I told her to "go away');
+
+let animal = "Dumbo octopus";
+
+console.log(animal[0]);
+
+// returns undefined
+console.log(animal[99]);
+
+let str = "Hello";
+str = -"";
+
+console.log(str);
+
+// string methods
+
+str = "abc";
+console.log(str.charAt(2));
+
+arr = [1, 2];
+
+console.log(arr[4]);
+
+str = "Hello";
+console.log(str.slice(-2));
+console.log(str.substring(-2));
