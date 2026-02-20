19cba884df981549d967255e8e40cd8ac653ab4e
gagan <gasingla07@gmail.com>
2026-02-20 13:30:49 -0600

    add implementation of data types

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,28 +1,48 @@
-let js = "amazing";
-console.log(40 + 8 + 23);
+// let js = "amazing";
+// console.log(40 + 8 + 23);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
 
-let $name = "Gagan";
-let _lastname = "Singla";
-console.log(_lastname);
-console.log($name);
+// let $name = "Gagan";
+// let _lastname = "Singla";
+// console.log(_lastname);
+// console.log($name);
 
-let new = 27;
+// let new = 27;
 
-let name = "Jonas";
+// let name = "Jonas";
 
-let Person = "John";
-console.log(Person);
+// let Person = "John";
+// console.log(Person);
 
-let PI = 3.1415;
+// let PI = 3.1415;
 
-console.log(PI);
+// console.log(PI);
 
-let myFirstJob = "Programmer";
-let myCurrentJob = 'teacher';
+// let myFirstJob = "Programmer";
+// let myCurrentJob = 'teacher';
 
+console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Gagan');
+console.log(typeof undefined);
+console.log(typeof null);
+
+javascriptIsFun = 'YES';
+
+console.log(typeof BigInt);
+
+let year;
+console.log(year);
+console.log(typeof year);
