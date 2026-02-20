97709841f5af0e112f0c807d277a02e640d07433
gagan <gasingla07@gmail.com>
2026-02-20 16:14:48 -0600

    add implementation of basic operators

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -26,41 +26,70 @@
 // let myFirstJob = "Programmer";
 // let myCurrentJob = 'teacher';
 
-console.log(true);
+// console.log(true);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof 'Gagan');
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Gagan');
+// console.log(typeof undefined);
+// console.log(typeof null);
 
-javascriptIsFun = 'YES';
+// javascriptIsFun = 'YES';
 
-console.log(typeof BigInt);
+// console.log(typeof BigInt);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-// const and var variables
+// // const and var variables
 
-let age= 30
-age = 31;
+// let age= 30
+// age = 31;
 
-// const birthYear = 1994;
-// birthYear = 1995
+// // const birthYear = 1994;
+// // birthYear = 1995
 
-//empty const variables are not allowed
-//const job;
+// //empty const variables are not allowed
+// //const job;
 
-var job = 'programmer';
-job = 'teacher';
+// var job = 'programmer';
+// job = 'teacher';
 
-lastName = 'Singla';
-console.log(lastName);
+// lastName = 'Singla';
+// console.log(lastName);
 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+
+console.log(ageJonas * 2, ageJonas /2, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Singh";
+
+console.log(firstName + " " + lastName);
+
+//assignment
+
+let x = 10 + 5;
+ x += 10;
+ x *= 4;
+ x++
+
+ x = 1 + x++
+console.log(x);
+
+
+//Comparison
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+console.log(ageSarah < 19);
