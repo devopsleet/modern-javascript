d1abcf64a9f928912c169ca71d424313052b7e5a
gagan <gasingla07@gmail.com>
2026-02-26 16:00:52 -0600

    add implementation of ternary operator

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -192,32 +192,53 @@
 
 // age = 16;
 
-const hasDriverLicence = true;
-const hasGoodVision = true;
+// const hasDriverLicence = true;
+// const hasGoodVision = true;
 
-console.log(hasDriverLicence && hasGoodVision);
+// console.log(hasDriverLicence && hasGoodVision);
 
-const day = 'monday';
+// const day = 'monday';
 
-switch(day) {
-    case 'monday':
-        console.log('Plan my course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    case 'wednesday':
-    case 'thursday':
-        console.log('Write code examples');
-        break;
-    case 'friday':
-        console.log('Record videos');
-        break;
-    case 'saturday':
-        console.log('Enjoy the weekend');
-        break;
-    default:
-        console.log('Not a valid day!');
-        
+// switch(day) {
+//     case 'monday':
+//         console.log('Plan my course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//         console.log('Enjoy the weekend');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+
+// }
+
+
+const age = 23;
+age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water!');
+
+
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
 }
+
+
+console.log(drink2);
+
+console.log(`I like to drink ${ age >= 18 ? 'wine' : 'water'}`);
